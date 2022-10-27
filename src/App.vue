f193ef430467c43a1c621fec5664f379d87eb90b
Daniel <danieldossantosdacosta@gmail.com>
2022-10-26 23:37:51 -0300

    transforming the site to vuejs

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Top from "./components/Top.vue";
+import MainSectionVue from "./components/MainSection.vue";
+</script>
+
+<template>
+  <Top />
+  <MainSectionVue />
+</template>
---
+++ src/components/Top.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "Top",
+  data() {
+    return {
+      srcImg: "https://github.com/danielcosta12.png",
+      atl: "Foto de Daniel Costa",
+    };
+  },
+};
+</script>
+<template>
+  <header>
+    <div>
+      <img v-bind:src="srcImg" v-bind:alt="alt" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding-top: 32px;
+  height: calc(120px + 32px);
+}
+header div {
+  width: 120px;
+  margin: auto;
+  background-image: linear-gradient(
+    90deg,
+    #9572fc 0%,
+    #43e7ad 50.52%,
+    #e2d45c 100%
+  );
+  border-radius: 50%;
+  display: flex;
+  padding: 4px;
+  transition: transform 0.5s;
+}
+header img {
+  width: 100%;
+  border-radius: 50%;
+}
+header div:hover {
+  transform: scale(1.1);
+}
+</style>
